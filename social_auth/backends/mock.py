7615708e7dd3a70f9e4cb66998cc1824fc12893f
John Krauss <irving.krauss@gmail.com>
2012-09-28 11:03:13 -0400

    Debugging mock backend

--- social_auth/backends/mock.py
+++ social_auth/backends/mock.py
@@ -3,6 +3,9 @@ Mock backends.
 """
 from social_auth.backends import OAuthBackend, BaseOAuth2, USERNAME
 from django.contrib.auth import authenticate
+from urllib import urlencode
+
+import time
 
 class MockOAuth2Backend(OAuthBackend):
     name = 'mock-oauth2'
@@ -21,13 +24,41 @@ class MockOAuth2Backend(OAuthBackend):
 class MockOAuth2(BaseOAuth2):
     """Mock OAuth 2"""
     AUTH_BACKEND = MockOAuth2Backend
+    SETTINGS_KEY_NAME = 'MOCK_OAUTH2_CLIENT_KEY'
+    SETTINGS_SECRET_NAME = 'MOCK_OAUTH2_CLIENT_SECRET'
+    REDIRECT_STATE = False
+    #AUTHORIZATION_URL = '/oauth/callback/mock-oauth2/'
+
+    def auth_url(self):
+        """
+        Fake redirect URL -- goes straight to confirmation.
+        """
+        state = self.state_token()
+
+        # Store state in session for further request validation. The state
+        # value is passed as state parameter (as specified in OAuth2 spec), but
+        # also added to redirect_uri, that way we can still verify the request
+        # if the provider doesn't implement the state parameter.
+        self.request.session[self.AUTH_BACKEND.name + '_state'] = state
+
+        args = {
+            'state': state,
+            'code': 'foobar',
+            'redirect_uri': self.get_redirect_uri(state),
+            'response_type': 'code'
+        }
+        return '/oauth/callback/mock-oauth2/?' + urlencode(args)
 
     def auth_complete(self, *args, **kwargs):
         """
         Fake out the login completion.  This would be one request.
         """
+        time.sleep(1)
+
+        self.validate_state()
+
         response = {}
-        response.update(self.user_data(None, response))
+        response.update(self.user_data("access_token", response))
         kwargs.update({
             'auth': self,
             'response': response,
@@ -42,16 +73,28 @@ class MockOAuth2(BaseOAuth2):
 
         TODO: method for populating this data
         """
+        time.sleep(1)
         return {
-             "id": "00000000000000",
-             "email": "fred.example@gmail.com",
-             "fullname": "Fred Example",
-             "first_name": "Fred",
-             "last_name": "Example",
-             "picture": "https://lh5.googleusercontent.com/-2Sv-4bBMLLA/AAAAAAAAAAI/AAAAAAAAABo/bEG4kI2mG0I/photo.jpg",
-             "gender": "male",
-             "locale": "en-US"
+            "access_token": access_token,
+            "expires_in": 3600,
+            "expires": 3600,
+            "id": "00000000000000",
+            "email": "fred.example@gmail.com",
+            "fullname": "Fred Example",
+            "first_name": "Fred",
+            "last_name": "Example",
+            "picture": "https://lh5.googleusercontent.com/-2Sv-4bBMLLA/AAAAAAAAAAI/AAAAAAAAABo/bEG4kI2mG0I/photo.jpg",
+            "gender": "male",
+            "locale": "en-US"
         }
+
+    @classmethod
+    def enabled(self):
+        """
+        TODO: only enable this in debug mode?
+        """
+        return True
+
 
 BACKENDS = {
     'mock-oauth2': MockOAuth2
